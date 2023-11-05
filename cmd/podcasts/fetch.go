2ed8f18c3c37ae16f81227cd5d70ab18534ef677
Anton Moilanen <anton.a.moilanen@gmail.com>
2023-11-06 01:59:13 +0400

    Implement fetching of the episodes from a feed

--- cmd/podcasts/fetch.go
+++ cmd/podcasts/fetch.go
@@ -2,7 +2,13 @@ package podcasts
 
 import (
 	"fmt"
+	"io"
+	"net/http"
+	"net/url"
 	"os"
+	"path"
+	"path/filepath"
+	"strings"
 
 	"github.com/amoilanen/gopodder/pkg/feed"
 	"github.com/spf13/cobra"
@@ -19,15 +25,77 @@ var useTitlesAsFileNames bool
 var downloadDirectory string
 
 func init() {
+	userHomeDir, err := os.UserHomeDir()
+	if err != nil {
+		panic(err)
+	}
+	defaultDownloadDirectory := filepath.Join(userHomeDir, "gopodder")
+
 	fetchCmd.Flags().BoolVarP(&useTitlesAsFileNames, "titles-as-file-names", "t", true, "Specifies that episode titles should be used as file names when downloading episodes")
-	fetchCmd.Flags().StringVarP(&downloadDirectory, "download-dir", "d", "~/gopodder", "Directory where the episodes should be saved")
+	fetchCmd.Flags().StringVarP(&downloadDirectory, "download-dir", "d", defaultDownloadDirectory, "Directory where the episodes should be saved")
+}
+
+// TODO: Extract downloadFile into a separate package
+func downloadFile(url, outputPath string) error {
+	response, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return fmt.Errorf("HTTP request failed with status code %d", response.StatusCode)
+	}
+	outFile, err := os.Create(outputPath)
+	if err != nil {
+		return err
+	}
+	defer outFile.Close()
+	contentLength := response.ContentLength
+
+	progressBar := NewProgressBar(contentLength)
+	writer := io.MultiWriter(outFile, progressBar)
+
+	_, err = io.Copy(writer, response.Body)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// TODO: Extract ProgressBar into a separate package
+func NewProgressBar(total int64) *ProgressBar {
+	return &ProgressBar{total: total}
+}
+
+type ProgressBar struct {
+	io.Writer
+	total       int64
+	bytesCopied int64
+}
+
+func (p *ProgressBar) Write(b []byte) (n int, err error) {
+	n = len(b)
+	p.bytesCopied += int64(n)
+
+	//TODO: Show the download speed
+	if p.total > 0 {
+		progress := float64(p.bytesCopied) / float64(p.total)
+		fmt.Printf("\r[%-80s] %.2f%%", strings.Repeat("#", int(progress*80)), progress*100)
+		if p.bytesCopied >= p.total {
+			fmt.Printf("\r %-100s", strings.Repeat(" ", 100))
+			fmt.Printf("\r")
+		}
+	} else {
+		fmt.Printf("\rDownloaded %d bytes", p.bytesCopied)
+	}
+
+	return n, nil
 }
 
 func runFetchCmd(ccmd *cobra.Command, args []string) {
 	//TODO: Implement fetching the episodes and also checking which episodes have already been fetched
 	println("Fetching feed...")
-	println(fmt.Sprintf("use titles as file names = %t, download directory = %s", useTitlesAsFileNames, downloadDirectory))
-	//TODO: Create downloadDirectory if it does not exist
 	feedUrl := args[0]
 	feedReader := feed.FeedReader{}
 	feed, err := feedReader.GetFeed(feedUrl)
@@ -35,6 +103,20 @@ func runFetchCmd(ccmd *cobra.Command, args []string) {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-	firstItem := *feed.Items[0]
-	fmt.Println(firstItem)
+	feedPath := filepath.Join(downloadDirectory, feed.Title)
+	if _, err := os.Stat(feedPath); os.IsNotExist(err) {
+		os.MkdirAll(feedPath, 0700)
+	}
+	//TODO: Show the overall progress, which episode of how many episodes is being downloaded, etc.
+	for _, episode := range feed.Items {
+		url, err := url.Parse(episode.Enclosure.Url)
+		if err != nil {
+			println(fmt.Errorf("Not parsable url %s", episode.Enclosure.Url))
+		}
+		name := episode.Title + path.Ext(url.Path)
+		fmt.Printf("Downloading \"%s\": episode \"%s\"\n", feed.Title, name)
+		downloadFile(url.String(), filepath.Join(feedPath, name))
+	}
+	//firstItem := *feed.Items[0]
+	//fmt.Println(firstItem)
 }
