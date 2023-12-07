a0a358295c35b5b008e8000c6deaf172697f95e5
Anton Moilanen <anton.a.moilanen@gmail.com>
2023-12-08 00:19:54 +0300

    Implement resuming download of a partially downloaded episode

--- pkg/http/HttpClient.go
+++ pkg/http/HttpClient.go
@@ -23,29 +23,75 @@ func (r *HttpClient) GetBytesFromUrl(url string) ([]byte, error) {
 	return respBody, nil
 }
 
+func getCurrentOutputSize(outputPath string) (int64, error) {
+	if fileInfo, err := os.Stat(outputPath); err == nil {
+		size := fileInfo.Size()
+		return size, nil
+	} else if os.IsNotExist(err) {
+		return 0, nil
+	} else {
+		return 0, fmt.Errorf("Error trying to check if the file %s is empty %v", outputPath, err)
+	}
+}
+
 func (r *HttpClient) DownloadFile(url, outputPath string) error {
-	response, err := http.Get(url)
+	var err error
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+
+	currentOutputSize, err := getCurrentOutputSize(outputPath)
+	existsNonEmptyOutput := currentOutputSize > 0 && err == nil
+
+	var outFile *os.File
+	if existsNonEmptyOutput {
+		outFile, err = os.OpenFile(outputPath, os.O_APPEND|os.O_WRONLY, 0644)
+	} else {
+		outFile, err = os.Create(outputPath)
+	}
+	if err != nil {
+		return err
+	}
+	defer outFile.Close()
+
+	if existsNonEmptyOutput {
+		req.Header.Set("Range", fmt.Sprintf("bytes=%d-", currentOutputSize))
+	}
+
+	response, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return err
 	}
 	defer response.Body.Close()
 
-	if response.StatusCode != http.StatusOK {
-		return fmt.Errorf("HTTP request failed with status code %d", response.StatusCode)
+	if existsNonEmptyOutput {
+		if response.StatusCode == http.StatusPartialContent {
+			fmt.Printf("Resuming download of file %s, current size %d\n", outputPath, currentOutputSize)
+		} else if response.StatusCode == http.StatusOK {
+			fmt.Printf("Already fully downloaded %s\n", outputPath)
+		} else {
+			fmt.Printf("Removing partially downloaded file = %s since server does not support resuming downloads\n", outputPath)
+			err := os.Remove(outputPath)
+			if err != nil {
+				return r.DownloadFile(url, outputPath)
+			}
+		}
+	} else {
+		if response.StatusCode != http.StatusOK {
+			return fmt.Errorf("HTTP request failed with status code %d", response.StatusCode)
+		}
 	}
-	outFile, err := os.Create(outputPath)
-	if err != nil {
-		return err
-	}
-	defer outFile.Close()
 	contentLength := response.ContentLength
 
-	progressBar := NewDownloadProgressBar(contentLength)
-	writer := io.MultiWriter(outFile, progressBar)
-
-	_, err = io.Copy(writer, response.Body)
-	if err != nil {
-		return err
+	if contentLength > currentOutputSize {
+		progressBar := NewDownloadProgressBar(contentLength)
+		progressBar.OnProgress(currentOutputSize)
+		writer := io.MultiWriter(outFile, progressBar)
+		_, err = io.Copy(writer, response.Body)
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
